86bd660c08753cf5d5fd127b9167675e347afc2b
Warlord <201951087@iiitvadodara.ac.in>
2021-11-14 18:07:51 +0530

    first commit

---
+++ maskDetector/views.py
@@ -0,0 +1,12 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'base.html')
+
+
+def about(request):
+    return render(request, 'about.html')
